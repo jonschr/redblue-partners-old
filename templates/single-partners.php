01d3a597e1ce81788ab6c19b88e0735528c2731f
Jon Schroeder <jon@redblue.us>
2015-09-10 11:34:12 -0500

    Remove the post info on the single template.

--- templates/single-partners.php
+++ templates/single-partners.php
@@ -1,13 +1,16 @@
 <?php
 
-//* Remove the post info function
-remove_action( 'genesis_entry_header', 'genesis_post_info', 5 );
+# Force full width content
+add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
 
 add_action( 'genesis_entry_content', 'rbp_add_featured_image', 0 );
 function rbp_add_featured_image() {
 	$img = genesis_get_image( array( 'format' => 'html', 'size' => 'partner-image', 'attr' => array( 'class' => 'alignright' ) ) );
 	printf( '%s', $img );
 }
+
+// Remove post info
+remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
 
 //* Remove the post meta function
 remove_action( 'genesis_entry_footer', 'genesis_post_meta', 10 );
